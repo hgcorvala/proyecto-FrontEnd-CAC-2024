2321e5e503b47de450babdc36f0036c6e2a60b02
emaenriquez <emaenriquez1985@gmail.com>
2024-05-21 01:21:56 -0300

    mejoras en el diseño de la pagina

--- js/formulario.js
+++ js/formulario.js
@@ -1,5 +1,46 @@
-// ↓↓↓↓↓↓↓____ Esto solo aplica clases de estilos, no afecta el funcionamiento ____ ↓↓↓↓↓↓↓
-$(document).ready(function(){
-	$("textarea").addClass("materialize-textarea")
-	$("input[type='submit']").addClass("btn waves-effect waves-light")
-})
+
+const name = document.getElementById('name').value;
+
+const email = document.getElementById('email').value;
+const age = document.getElementById('age').value;
+const gender = document.getElementById('gender').value;
+const mensaje = document.getElementById('mensaje').value;
+function validateForm() {
+	let isValid = true;
+	if (name.trim() === '') {
+		document.getElementById('nameError').innerText = 'El nombre es obligatorio';
+		isValid = false;
+	} else {
+		document.getElementById('nameError').innerText = '';
+	}
+
+	if (email.trim() === '') {
+		document.getElementById('emailError').innerText = 'El correo electrónico es obligatorio';
+		isValid = false;
+	} else {
+		document.getElementById('emailError').innerText = '';
+	}
+
+	if (age.trim() === '' || isNaN(age) || age <= 0) {
+		document.getElementById('ageError').innerText = 'La edad es obligatoria y debe ser un número positivo';
+		isValid = false;
+	} else {
+		document.getElementById('ageError').innerText = '';
+	}
+
+	if (gender === '') {
+		document.getElementById('genderError').innerText = 'El género es obligatorio';
+		isValid = false;
+	} else {
+		document.getElementById('genderError').innerText = '';
+	}
+
+	if (mensaje.trim() === '') {
+		document.getElementById('mensajeError').innerText = 'El mensaje es obligatorio';
+		isValid = false;
+	} else {
+		document.getElementById('mensajeError').innerText = '';
+	}
+
+	return isValid;
+}
--- js/productos.js
+++ js/productos.js
@@ -4,17 +4,17 @@ class Producto{
         this.idProducto = idProductoValue;
         this.nombreProducto = nombreProductoValue;
         this.precioProducto = precioProductoValue;  
-        this.descripcionProdcuto = descripcionProductoValue;    
+        this.descripcionProducto = descripcionProductoValue;
     }
 }
 
 //Definimos los productos que hay en stock y se mostraran en la pagina de mandera dinamica
-producto1 = new Producto(1, 'Oleo para Masajes', '$1000', 'Para masajes en todo el cuerpo');
-producto2 = new Producto(2, 'Aceite de Canabis', '$1500', 'Para calmar dolores articulares');
-producto3 = new Producto(3, 'Mascarilla facial de carbon', '$2700', 'Para evitar las arrugas');
-producto4 = new Producto(4, 'Aceite de Magnesio', '$3000', 'Para nutrir la piel');
-producto5 = new Producto(5, 'Blanqueador dental ecologico', '$1500', 'Para tener los dientes bien blancos y limpios');
-producto6 = new Producto(6, 'Crema Facial AntiAge', '$5000', 'Atrada el envejecimiento de la piel');
+let producto1 = new Producto(1, 'Oleo para Masajes', '$1000', 'Para masajes en todo el cuerpo');
+let producto2 = new Producto(2, 'Aceite de Canabis', '$1500', 'Para calmar dolores articulares');
+let producto3 = new Producto(3, 'Mascarilla facial de carbon', '$2700', 'Para evitar las arrugas');
+let producto4 = new Producto(4, 'Aceite de Magnesio', '$3000', 'Para nutrir la piel');
+let producto5 = new Producto(5, 'Blanqueador dental ecologico', '$1500', 'Para tener los dientes bien blancos y limpios');
+let producto6 = new Producto(6, 'Crema Facial AntiAge', '$5000', 'Atrada el envejecimiento de la piel');
 
 
 const productos = [producto1, producto2, producto3, producto4, producto5, producto6];
@@ -24,7 +24,6 @@ function mostrarProductos(productos) {
     productos.forEach(producto => {
         const fila = document.createElement('tr');
         fila.innerHTML = `
-            <td>${producto.idProducto}</td>
             <td>${producto.nombreProducto}</td>
             <td>${producto.precioProducto}</td>
             <td>${producto.descripcionProducto}</td>
